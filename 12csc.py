d30b9e717d94cbd38ffa2038d58272eb3792d95e
23065 <23065@students.mrgs.school.nz>
2026-05-04 10:35:28 +1200

    Add files via upload

---
+++ 12csc.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+quiz_data = [
+    {"q":"What section of a car is this?", "options": ["Transmission", "Brake Pad", "Engine", "Exhaust"], "a": "Transmission"},
+    {"q":"What section of a car is this?", "options": ["Engine", "Rim", "Accelerator", "Tyre"], "a": "Rim"},
+    {"q":"What section of a car is this?", "options": ["Brake", "Foot Brake", "Accelerator", "Hand Brake"], "a": "Foot Brake"},
+    {"q":"What section of a car is this?", "options": ["Engine", "Accelerator", "Coolant Tank", "Oil Tank"], "a": "Oil Tank"},
+    {"q":"What section of a car is this?", "options": ["Coolant Tank", "Dip Stick", "Engine", "Oil Tank"], "a": "Engine"},
+]
+
