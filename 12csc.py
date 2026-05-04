83efc30d23326bd7eba83705c047000275ab6f5f
23065 <23065@students.mrgs.school.nz>
2026-05-05 09:42:35 +1200

    12csc.py

--- 12csc.py
+++ 12csc.py
@@ -1,10 +1,24 @@
-import tkinter as tk
+import customtkinter as ctk
+from PIL import Image   
+import os
 
-quiz_data = [
-    {"q":"What section of a car is this?", "options": ["Transmission", "Brake Pad", "Engine", "Exhaust"], "a": "Transmission"},
-    {"q":"What section of a car is this?", "options": ["Engine", "Rim", "Accelerator", "Tyre"], "a": "Rim"},
-    {"q":"What section of a car is this?", "options": ["Brake", "Foot Brake", "Accelerator", "Hand Brake"], "a": "Foot Brake"},
-    {"q":"What section of a car is this?", "options": ["Engine", "Accelerator", "Coolant Tank", "Oil Tank"], "a": "Oil Tank"},
-    {"q":"What section of a car is this?", "options": ["Coolant Tank", "Dip Stick", "Engine", "Oil Tank"], "a": "Engine"},
-]
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
+    # COLOURS
+
+BG = "#0b1622"
+CARD = "#0f1c2b"
+BTN = "#2a3f55"
+BTN_HOVER = "#3b5670"
+SELECT = "#1f6aa5"
+TEXT_MAIN = "#e6edf3"
+TEXT_SUB = "#9fb3c8"
+GREEN = "#2ecc71"
+RED = "#e74c3c"
+
+questions = [{
+    "question": "What part of a car is this?",
+    "image": "transmission.jpg",
+    "options": ["Brake Pad", "Transmission", "Engine", "Exhaust"]
+}]
